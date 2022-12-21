e687a4c209ea0e8076eea690621c9e5bb2cb8fb1
alspin8 <46360108+alspin8@users.noreply.github.com>
2022-12-21 20:51:04 +0100

    Increase performance by replacing Map by array

--- src/main/java/fr/alspin/opendl/Main.java
+++ src/main/java/fr/alspin/opendl/Main.java
@@ -1,271 +1,13 @@
 package fr.alspin.opendl;
 
-
+import fr.alspin.opendl.math.Function;
 import fr.alspin.opendl.math.Matrix;
-import me.tongfei.progressbar.ProgressBar;
-import me.tongfei.progressbar.ProgressBarBuilder;
-import me.tongfei.progressbar.ProgressBarStyle;
 import org.math.plot.Plot2DPanel;
 import org.math.plot.PlotPanel;
-import org.math.plot.plots.ScatterPlot;
-
 import javax.swing.*;
-import java.awt.*;
 import java.util.Arrays;
 
 public class Main {
-
-    private static void progressBar() {
-        long iters = 100;
-        try (ProgressBar pb = new ProgressBarBuilder()
-                .setInitialMax(iters)
-                .setTaskName("Loop")
-                .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
-                .showSpeed()
-                .build()) {
-            for(int i = 1;i <= iters;i ++)
-            {
-                Thread.sleep(100);
-                pb.stepTo(i);
-
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void simpleMatrix() {
-        Matrix a = Matrix.random(5, 1);
-        Matrix b = Matrix.random(5, 1);
-        a.print(5, 3);
-        b.print(5, 3);
-        Matrix c = a.mul(b);
-        c.print(5, 3);
-
-        a.pShape();
-
-        Matrix mat = Matrix.random(10, 2);
-
-        Plot2DPanel plot = new Plot2DPanel();
-        plot.addLinePlot("mat", mat.array());
-
-        JFrame frame = new JFrame("matrix");
-        frame.setContentPane(plot);
-        frame.setVisible(true);
-    }
-
-//    private static void simpleGraph() {
-//        Matrix input_1 = Data.INPUT_TRAIN_SET_LINEAR, Data.OUTPUT_TRAIN_SET_LINEAR, 1);
-//        Matrix input_0 = Data.INPUT_TRAIN_SET_LINEAR, Data.OUTPUT_TRAIN_SET_LINEAR, 0);
-//
-//        Plot2DPanel plot = new Plot2DPanel("");
-//        plot.plotCanvas.addPlot(new ScatterPlot("True", Color.GREEN, 1, 5, input_1.array()));
-//        plot.plotCanvas.addPlot(new ScatterPlot("True", Color.RED, 1, 5, input_0.array()));
-//        plot.addLegend("South");
-//
-//        JFrame frame = new JFrame("matrix");
-//        frame.setSize(1200, 720);
-//        frame.setContentPane(plot);
-//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//        frame.setVisible(true);
-//    }
-
-    private static void onlyTrainSetNoLayersLinear(int nIt) {
-        NeuralNetwork nn = new NeuralNetwork(2, 1);
-
-        double[][] result = nn.train(Data.INPUT_TRAIN_SET_LINEAR.transpose(), Data.OUTPUT_TRAIN_SET_LINEAR.transpose(), 0.1, nIt).transpose().array();
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-
-        Plot2DPanel acc = new Plot2DPanel();
-        acc.addLinePlot("Accuracy", result[1]);
-
-        Plot2DPanel log = new Plot2DPanel();
-        log.addLinePlot("LogLoss", result[0]);
-
-        panel.add(acc);
-        panel.add(log);
-
-        JFrame frame = new JFrame("matrix");
-        frame.setSize(1200, 720);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setContentPane(panel);
-
-        acc.setFixedBounds(1, 0, 1);
-        log.setFixedBounds(1, 0, Arrays.stream(result[0]).max().getAsDouble());
-
-        frame.setVisible(true);
-    }
-
-    private static void trainAndTestSetNoLayersLinear(int nIt) {
-        NeuralNetwork nn = new NeuralNetwork(2, 1);
-
-        Matrix[] result = nn.train(
-                Data.INPUT_TRAIN_SET_LINEAR.transpose(),
-                Data.OUTPUT_TRAIN_SET_LINEAR.transpose(),
-                Data.INPUT_TEST_SET_LINEAR.transpose(),
-                Data.OUTPUT_TEST_SET_LINEAR.transpose(),
-                0.1, nIt);
-
-        double[][] trainingHistory = result[0].transpose().array();
-        double[][] testHistory = result[1].transpose().array();
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
-        JPanel trainingPanel = new JPanel();
-        trainingPanel.setLayout(new BoxLayout(trainingPanel, BoxLayout.X_AXIS));
-
-        JPanel testPanel = new JPanel();
-        testPanel.setLayout(new BoxLayout(testPanel, BoxLayout.X_AXIS));
-
-        mainPanel.add(trainingPanel);
-        mainPanel.add(testPanel);
-
-        Plot2DPanel trainingLog = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingLog.addLinePlot("Training", trainingHistory[0]);
-
-        Plot2DPanel trainingAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingAcc.addLinePlot("Training", trainingHistory[1]);
-
-        trainingPanel.add(trainingLog);
-        trainingPanel.add(trainingAcc);
-
-        Plot2DPanel testLog = new Plot2DPanel(PlotPanel.SOUTH);
-        testLog.addLinePlot("Test", testHistory[0]);
-
-        Plot2DPanel testAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        testAcc.addLinePlot("Test", testHistory[1]);
-
-        testPanel.add(testLog);
-        testPanel.add(testAcc);
-
-        JFrame frame = new JFrame("matrix");
-        frame.setSize(1200, 720);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setContentPane(mainPanel);
-
-        trainingLog.setFixedBounds(1, 0, Arrays.stream(trainingHistory[0]).max().getAsDouble());
-        trainingAcc.setFixedBounds(1, 0, 1);
-        testLog.setFixedBounds(1, 0, Arrays.stream(testHistory[0]).max().getAsDouble());
-        testAcc.setFixedBounds(1, 0, 1);
-
-        frame.setVisible(true);
-    }
-
-    private static void trainAndTestSetNoLayersCircle(int nIt) {
-        NeuralNetwork nn = new NeuralNetwork(2, 1);
-
-        Matrix[] result = nn.train(
-                Data.INPUT_TRAIN_SET_CIRCLE.transpose(),
-                Data.OUTPUT_TRAIN_SET_CIRCLE.transpose(),
-                Data.INPUT_TEST_SET_CIRCLE.transpose(),
-                Data.OUTPUT_TEST_SET_CIRCLE.transpose(),
-                0.1, nIt);
-
-        double[][] trainingHistory = result[0].transpose().array();
-        double[][] testHistory = result[1].transpose().array();
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
-        JPanel trainingPanel = new JPanel();
-        trainingPanel.setLayout(new BoxLayout(trainingPanel, BoxLayout.X_AXIS));
-
-        JPanel testPanel = new JPanel();
-        testPanel.setLayout(new BoxLayout(testPanel, BoxLayout.X_AXIS));
-
-        mainPanel.add(trainingPanel);
-        mainPanel.add(testPanel);
-
-        Plot2DPanel trainingLog = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingLog.addLinePlot("Training", trainingHistory[0]);
-
-        Plot2DPanel trainingAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingAcc.addLinePlot("Training", trainingHistory[1]);
-
-        trainingPanel.add(trainingLog);
-        trainingPanel.add(trainingAcc);
-
-        Plot2DPanel testLog = new Plot2DPanel(PlotPanel.SOUTH);
-        testLog.addLinePlot("Test", testHistory[0]);
-
-        Plot2DPanel testAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        testAcc.addLinePlot("Test", testHistory[1]);
-
-        testPanel.add(testLog);
-        testPanel.add(testAcc);
-
-        JFrame frame = new JFrame("matrix");
-        frame.setSize(1200, 720);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setContentPane(mainPanel);
-
-        trainingLog.setFixedBounds(1, 0, Arrays.stream(trainingHistory[0]).max().getAsDouble());
-        trainingAcc.setFixedBounds(1, 0, 1);
-        testLog.setFixedBounds(1, 0, Arrays.stream(testHistory[0]).max().getAsDouble());
-        testAcc.setFixedBounds(1, 0, 1);
-
-        frame.setVisible(true);
-    }
-
-    private static void trainAndTestSetCircle(int nIt, int[] layers) {
-        NeuralNetwork nn = new NeuralNetwork(layers, 2, 1);
-
-        Matrix[] result = nn.train(
-                Data.INPUT_TRAIN_SET_CIRCLE.transpose(),
-                Data.OUTPUT_TRAIN_SET_CIRCLE.transpose(),
-                Data.INPUT_TEST_SET_CIRCLE.transpose(),
-                Data.OUTPUT_TEST_SET_CIRCLE.transpose(),
-                0.1, nIt);
-
-        double[][] trainingHistory = result[0].transpose().array();
-        double[][] testHistory = result[1].transpose().array();
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
-        JPanel trainingPanel = new JPanel();
-        trainingPanel.setLayout(new BoxLayout(trainingPanel, BoxLayout.X_AXIS));
-
-        JPanel testPanel = new JPanel();
-        testPanel.setLayout(new BoxLayout(testPanel, BoxLayout.X_AXIS));
-
-        mainPanel.add(trainingPanel);
-        mainPanel.add(testPanel);
-
-        Plot2DPanel trainingLog = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingLog.addLinePlot("Training", trainingHistory[0]);
-
-        Plot2DPanel trainingAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        trainingAcc.addLinePlot("Training", trainingHistory[1]);
-
-        trainingPanel.add(trainingLog);
-        trainingPanel.add(trainingAcc);
-
-        Plot2DPanel testLog = new Plot2DPanel(PlotPanel.SOUTH);
-        testLog.addLinePlot("Test", testHistory[0]);
-
-        Plot2DPanel testAcc = new Plot2DPanel(PlotPanel.SOUTH);
-        testAcc.addLinePlot("Test", testHistory[1]);
-
-        testPanel.add(testLog);
-        testPanel.add(testAcc);
-
-        JFrame frame = new JFrame("matrix");
-        frame.setSize(1200, 720);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setContentPane(mainPanel);
-
-        trainingLog.setFixedBounds(1, 0, Arrays.stream(trainingHistory[0]).max().getAsDouble());
-        trainingAcc.setFixedBounds(1, 0, 1);
-        testLog.setFixedBounds(1, 0, Arrays.stream(testHistory[0]).max().getAsDouble());
-        testAcc.setFixedBounds(1, 0, 1);
-
-        frame.setVisible(true);
-    }
-
     private static void trainSetCircle(int nIt, int[] layers) {
         NeuralNetwork nn = new NeuralNetwork(layers, 2, 1);
 
@@ -273,6 +15,11 @@ public class Main {
                 Data.INPUT_TRAIN_SET_CIRCLE.transpose(),
                 Data.OUTPUT_TRAIN_SET_CIRCLE.transpose(),
                 0.1, nIt).transpose().array();
+
+        Matrix r2 = nn.predict(Data.INPUT_TEST_SET_CIRCLE.transpose());
+
+        double r3 = Function.accuracyScore(Data.OUTPUT_TEST_SET_CIRCLE.transpose().flatten(), r2.flatten());
+        System.out.println(r3);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
@@ -291,19 +38,15 @@ public class Main {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setContentPane(mainPanel);
 
-        trainingLog.setFixedBounds(1, 0, Arrays.stream(result[0]).max().getAsDouble());
+        if(Arrays.stream(result[0]).max().isPresent())
+            trainingLog.setFixedBounds(1, 0, Arrays.stream(result[0]).max().getAsDouble());
         trainingAcc.setFixedBounds(1, 0, 1);
 
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-//        trainAndTestSetNoLayersLinear(100000);
-//        trainAndTestSetNoLayersCircle(10000);
-//        trainAndTestSetCircle(3000, new int[] {32});
-//        trainAndTestSetCircle(10000, new int[] {16, 16, 16});
-//        trainAndTestSetCircle(1, new int[] {4, 4});
-        trainSetCircle(3000, new int[] {16, 16, 16});
-//        Test.matrixTest();
+//        trainSetCircle(1000, new int[] {16, 16, 16});
+        trainSetCircle(10000, new int[] {16});
     }
 }
--- src/main/java/fr/alspin/opendl/NeuralNetwork.java
+++ src/main/java/fr/alspin/opendl/NeuralNetwork.java
@@ -2,6 +2,7 @@ package fr.alspin.opendl;
 
 import fr.alspin.opendl.math.Matrix;
 import fr.alspin.opendl.math.Function;
+import fr.alspin.opendl.util.ProgressBarWrapper;
 import me.tongfei.progressbar.ProgressBar;
 import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
@@ -12,143 +13,100 @@ import java.util.function.Consumer;
 
 public class NeuralNetwork {
 
-    private final List<Integer> dimensions = new ArrayList<>();
-    private final Map<String, Matrix> parameters = new HashMap<>();
-    private final Map<String, Matrix> activations = new HashMap<>();
-    private final Map<String, Matrix> gradients = new HashMap<>();
-
     private int nNeuron;
+    private Matrix[][] parameters;
+    private Matrix[] activations;
+    private Matrix[][] gradients;
 
     public NeuralNetwork(int[] layers, int nInput, int nOutput) {
-        this.dimensions.add(nInput);
-        this.dimensions.addAll(Arrays.stream(layers).boxed().toList());
-        this.dimensions.add(nOutput);
+        List<Integer> dimensions = new ArrayList<>();
+        dimensions.add(nInput);
+        dimensions.addAll(Arrays.stream(layers).boxed().toList());
+        dimensions.add(nOutput);
 
-        this.initialisation();
+        this.initialisation(dimensions);
     }
 
     public NeuralNetwork(int nInput, int nOutput) {
-        this.dimensions.add(nInput);
-        this.dimensions.add(nOutput);
+        List<Integer> dimensions = new ArrayList<>();
+        dimensions.add(nInput);
+        dimensions.add(nOutput);
 
-        this.initialisation();
+        this.initialisation(dimensions);
     }
 
-    private void initialisation() {
-        for (int i = 1; i < this.dimensions.size(); i++) {
-            this.parameters.put("W" + i, Matrix.gaussian(this.dimensions.get(i), this.dimensions.get(i - 1)));
-            this.parameters.put("b" + i, Matrix.gaussian(this.dimensions.get(i), 1));
+    private void initialisation(List<Integer> dims) {
+        this.parameters = new Matrix[2][dims.size()];
+        this.gradients = new Matrix[2][dims.size()];
+
+        this.nNeuron = Math.floorDiv(2 * (dims.size() - 1), 2);
+        this.activations = new Matrix[this.nNeuron + 1];
+
+        for (int i = 1; i < dims.size(); i++) {
+            this.parameters[0][i] = Matrix.gaussian(dims.get(i), dims.get(i - 1));
+            this.parameters[1][i] = Matrix.gaussian(dims.get(i), 1);
         }
-        this.nNeuron = Math.floorDiv(this.parameters.size(), 2);
     }
 
     private void forwardPropagation(Matrix input) {
-        if(this.activations.putIfAbsent("A0", input) != null)
-            this.activations.replace("A0", input);
+        this.activations[0] = input;
 
         for (int i = 1; i <= this.nNeuron ; i++) {
-            Matrix Z = this.parameters.get("W" + i).dot(this.activations.get("A" + (i - 1))).add(this.parameters.get("b" + i));
-
-            if(this.activations.putIfAbsent("A" + i, Function.sigmoid(Z)) != null)
-                this.activations.replace("A" + i, Function.sigmoid(Z));
+            Matrix Z = this.parameters[0][i].dot(this.activations[i - 1]).add(this.parameters[1][i]);
+            this.activations[i] = Function.sigmoid(Z);
         }
     }
 
     private void backPropagation(@NotNull Matrix output) {
-        double m = output.shape()[1];
-        Matrix dZ = this.activations.get("A" + nNeuron).sub(output);
+        double m = output.n();
+
+        Matrix dZ = this.activations[this.nNeuron].sub(output);
 
         for (int i = this.nNeuron; i > 0; i--) {
-            if(this.gradients.putIfAbsent("dW" + i, dZ.dot(this.activations.get("A" + (i - 1)).transpose()).mul(1.0 / m)) != null) {
-                this.gradients.replace("dW" + i, dZ.dot(this.activations.get("A" + (i - 1)).transpose()).mul(1.0 / m));
-            }
-            if(this.gradients.putIfAbsent("db" + i, dZ.sum(Matrix.Axis.ROW).mul(1.0 / m)) != null) {
-                this.gradients.replace("db" + i, dZ.sum(Matrix.Axis.ROW).mul(1.0 / m));
-            }
+            this.gradients[0][i] = dZ.dot(this.activations[i - 1].transpose()).mul(1.0 / m);
+            this.gradients[1][i] = dZ.sum(Matrix.Axis.ROW).mul(1.0 / m);
+
             if(i > 1) {
-                dZ = this.parameters.get("W" + i)
+                dZ = this.parameters[0][i]
                         .transpose()
                         .dot(dZ)
-                        .mul(this.activations.get("A" + (i - 1))
-                        .mul(this.activations.get("A" + (i - 1)).opposite().add(1)));
+                        .mul(this.activations[i - 1])
+                        .mul(this.activations[i - 1].opposite().add(1));
             }
         }
     }
 
     private void update(double learningRate) {
         for (int i = 1; i <= this.nNeuron; i++) {
-            this.parameters.replace("W" + i, this.parameters.get("W" + i).sub(this.gradients.get("dW" + i).mul(learningRate)));
-            this.parameters.replace("b" + i, this.parameters.get("b" + i).sub(this.gradients.get("db" + i).mul(learningRate)));
+            this.parameters[0][i] = this.parameters[0][i].sub(this.gradients[0][i].mul(learningRate));
+            this.parameters[1][i] = this.parameters[1][i].sub(this.gradients[1][i].mul(learningRate));
         }
     }
 
     public Matrix predict(Matrix input) {
         forwardPropagation(input);
-        return this.activations.get("A" + this.nNeuron).compare((val) -> val >= 0.5 ? 1. : 0.);
+        return this.activations[this.nNeuron].compare((val) -> val >= 0.5 ? 1. : 0.);
     }
 
-    private Matrix[] predictAndActivation(Matrix input) {
-        return new Matrix[] {predict(input), this.activations.get("A" + this.nNeuron)};
-    }
+    public Matrix train(Matrix inputTrainSet, Matrix outputTrainSet, double learningRate, int nIteration) {
+        nIteration = Math.max(nIteration, 1000);
+        Matrix trainingHistory = new Matrix(1000, 2, 0);
 
-    public Matrix train(Matrix inputTrainSet, Matrix outputTrainSet, double learningRate, int nIteration, Consumer<Integer> consumer) {
-        Matrix trainingHistory = new Matrix(Math.min(nIteration, 1000), 2, 0);
-        Matrix outputPrediction;
-        try (ProgressBar pb = new ProgressBarBuilder()
-                .setInitialMax(nIteration)
-                .setTaskName("Loop")
-                .setStyle(ProgressBarStyle.COLORFUL_UNICODE_BLOCK)
-                .showSpeed()
-                .build()) {
-            for (int i = 1; i <= nIteration; i++) {
+        ProgressBarWrapper.wrap(nIteration, (pb, iter) -> {
+            for (int i = 1; i <= iter; i++) {
+
                 forwardPropagation(inputTrainSet);
                 backPropagation(outputTrainSet);
                 update(learningRate);
 
-                if(nIteration > 1000) {
-                    if(i % (nIteration / 1000) == 0) {
-                        trainingHistory.set((i - 1) / (nIteration / 1000), 0, Function.logLoss(outputTrainSet.flatten(), activations.get("A" + this.nNeuron).flatten()));
-                        outputPrediction = predict(inputTrainSet);
-                        trainingHistory.set((i - 1) / (nIteration / 1000), 1, Function.accuracyScore(outputTrainSet.flatten(), outputPrediction.flatten()));
-                        if(consumer != null)
-                            consumer.accept(i);
-                    }
-                } else {
-                    trainingHistory.set(i - 1, 0, Function.logLoss(outputTrainSet.flatten(), activations.get("A" + this.nNeuron).flatten()));
-                    outputPrediction = predict(inputTrainSet);
-                    trainingHistory.set(i - 1, 1, Function.accuracyScore(outputTrainSet.flatten(), outputPrediction.flatten()));
-                    if(consumer != null)
-                        consumer.accept(i);
+                if(i % (iter / 1000) == 0) {
+                    trainingHistory.set((i - 1) / (iter / 1000), 0, Function.logLoss(outputTrainSet.flatten(), this.activations[this.nNeuron].flatten()));
+                    trainingHistory.set((i - 1) / (iter / 1000), 1, Function.accuracyScore(outputTrainSet.flatten(), predict(inputTrainSet).flatten()));
                 }
-                if(consumer != null)
-                    consumer.accept(i);
 
                 pb.stepTo(i);
             }
-        }
-        return trainingHistory;
-    }
-
-    public Matrix train(Matrix inputTrainSet, Matrix outputTrainSet, double learningRate, int nIteration) {
-        return train(inputTrainSet, outputTrainSet, learningRate, nIteration, null);
-    }
-
-    public Matrix[] train(Matrix inputTrainSet, Matrix outputTrainSet, Matrix inputTestSet, Matrix outputTestSet, double learningRate, int nIteration) {
-        Matrix testingHistory = new Matrix(Math.min(nIteration, 1000), 2, 0);
-        Matrix trainingHistory = train(inputTrainSet, outputTrainSet, learningRate, nIteration, (i) -> {
-            Matrix[] outputPrediction;
-            if(nIteration > 1000) {
-                if(i % (nIteration / 1000) == 0) {
-                    outputPrediction = predictAndActivation(inputTestSet);
-                    testingHistory.set((i - 1) / (nIteration / 1000), 0, Function.logLoss(outputTestSet.flatten(), outputPrediction[1].flatten()));
-                    testingHistory.set((i - 1) / (nIteration / 1000), 1, Function.accuracyScore(outputTestSet.flatten(), outputPrediction[0].flatten()));
-                }
-            } else {
-                outputPrediction = predictAndActivation(inputTestSet);
-                testingHistory.set(i - 1, 0, Function.logLoss(outputTestSet.flatten(), outputPrediction[1].flatten()));
-                testingHistory.set(i - 1, 1, Function.accuracyScore(outputTestSet.flatten(), outputPrediction[0].flatten()));
-            }
         });
-        return new Matrix[] {trainingHistory, testingHistory};
+        return trainingHistory;
     }
 }
